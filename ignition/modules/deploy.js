14749107faabe2e6f79971b056b67b94dfde4d41
devsamz tol <samueldahunsi3@gmail.com>
2024-07-26 05:47:01 +0000

    initial commit

--- ignition/modules/deploy.js
+++ ignition/modules/deploy.js
@@ -10,7 +10,6 @@ async function main() {
 
   // Display the contract address
   console.log(`degen token deployed to ${degen.target}`);
-  // 0x2F68427a28BBCDFB9EA095Da40543C311c4D9b55
 }
 
 // Hardhat recommends this pattern to be able to use async/await everywhere
